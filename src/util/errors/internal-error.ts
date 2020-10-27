8d74f8ced43049b11f8e74d79371f371dc6abdc5
Gabriel <gaoliveira211999@gmail.com>
2020-10-27 11:13:13 -0300

    refactor api client

--- src/util/errors/internal-error.ts
+++ src/util/errors/internal-error.ts
@@ -6,6 +6,6 @@ export class InternalError extends Error {
   ) {
     super(message);
     this.name = this.constructor.name;
-    Error.captureStackTrace(this, this.constructor)
+    Error.captureStackTrace(this, this.constructor);
   }
 }
