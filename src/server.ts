af0874f2902c586cdaf514ffc8954f58118c1e35
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-11-09 15:38:54 -0300

    openapi

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,14 @@ import './util/module-alias';
 import * as database from './database';
 import expressPino from 'express-pino-logger';
 import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import { OpenApiValidator } from 'express-openapi-validator';
+import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types';
 import { ForecastController } from './controllers/forecast';
 import { BeachesController } from './controllers/beaches';
 import { UsersController } from './controllers/users';
 import logger from './logger';
+import apiSchema from './api-schema.json';
 
 export class SetupServer extends Server {
   constructor(private port = 3333) {
@@ -17,6 +21,7 @@ export class SetupServer extends Server {
 
   public async init(): Promise<void> {
     this.setupExpress();
+    await this.docsSetup();
     this.setupControllers();
     await this.databaseSetup();
   }
@@ -62,5 +67,14 @@ export class SetupServer extends Server {
 
   private async databaseSetup(): Promise<void> {
     await database.connect();
+  }
+
+  private async docsSetup(): Promise<void> {
+    this.app.use('/docs', swaggerUI.serve, swaggerUI.setup(apiSchema));
+    await new OpenApiValidator({
+      apiSpec: apiSchema as OpenAPIV3.Document,
+      validateRequests: true,
+      validateResponses: true,
+    }).install(this.app);
   }
 }
